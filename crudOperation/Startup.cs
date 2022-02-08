09634bdf64a9d06b199e2ddeee2afaba1bc62cc4
M N Tushar <M N Tushar@DESKTOP-N9L89RA>
2022-02-08 12:44:23 +0600

    Add project files.

---
+++ crudOperation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(crudOperation.Startup))]
+namespace crudOperation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
