1dce4611e10410dc399c4ef89c24ce4180303993
M N Tushar <mntushar25@gmail.com>
2022-02-16 23:57:27 +0600

    required in department

---
+++ crudOperation/Migrations/202202161537325_addRequiredInDevelopmen.cs
@@ -0,0 +1,20 @@
+﻿namespace crudOperation.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addRequiredInDevelopmen : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.DepartmentModels", "Name", c => c.String(nullable: false));
+            AlterColumn("dbo.DepartmentModels", "Code", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.DepartmentModels", "Code", c => c.String());
+            AlterColumn("dbo.DepartmentModels", "Name", c => c.String());
+        }
+    }
+}
